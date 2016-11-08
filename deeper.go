95072fde172d1617901cb24a9d1d68c9b20bdd54
Arnaldur <aarnaldur@gmail.com>
2016-11-08 23:29:11 +0000

    SDL > ebiten

--- deeper.go
+++ deeper.go
@@ -1,11 +1,9 @@
 package main
 
-import (
-	"fmt"
-)
-
+// import (
+// 	"github.com/veandco/go-sdl2/sdl"
+// )
 
 func main() {
-	display()
-	fmt.Println("Hello World!")
+	startRenderLoop()
 }
--- display.go
+++ display.go
@@ -1,15 +1,57 @@
 package main
 
 import (
-	"github.com/hajimehoshi/ebiten"
-	"github.com/hajimehoshi/ebiten/ebitenutil"
+	"github.com/veandco/go-sdl2/sdl"
+	"github.com/veandco/go-sdl2/sdl_gfx"
 )
 
-func update(screen *ebiten.Image) error {
-	ebitenutil.DebugPrint(screen, "Hello world!")
-	return nil
+const (
+	MAINMENU = iota
+)
+
+func startRenderLoop() {
+	sdl.Init(sdl.INIT_EVERYTHING)
+	//	gamestate := MAINMENU
+	running := true
+	var fpsManager gfx.FPSmanager
+
+	window, err := sdl.CreateWindow("Go deeper", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
+		800, 600, sdl.WINDOW_SHOWN)
+	if err != nil {
+		panic(err)
+	}
+	defer window.Destroy()
+
+	gfx.InitFramerate(&fpsManager)
+	gfx.SetFramerate(&fpsManager, 60)
+
+	surface, err := window.GetSurface()
+	if err != nil {
+		panic(err)
+	}
+
+	var i int32 = 0
+	for running {
+		running = processInputs()
+		surface.FillRect(&sdl.Rect{0, 0, 800, 600}, 0xff000000)
+		rect := sdl.Rect{0, 0, 200, 200}
+		surface.FillRect(&rect, 0xffff0000)
+		surface.FillRect(&sdl.Rect{2 * i, i, 400, 400}, 0xff00ff00)
+		window.UpdateSurface()
+		i++
+		gfx.FramerateDelay(&fpsManager)
+	}
+
+	sdl.Quit()
 }
 
-func display() {
-	ebiten.Run(update, 320, 240, 2, "Hello world!")
+func processInputs() bool {
+	var event sdl.Event
+	for event = sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+		switch event.(type) {
+		case *sdl.QuitEvent:
+			return false
+		}
+	}
+	return true
 }
