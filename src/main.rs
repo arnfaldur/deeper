38e9f2267cc67d44514890068014cd331d1128a1
Jökull Reynisson <jokullmani12@gmail.com>
2020-02-21 22:10:46 +0000

    minor tweaks to example

--- src/main.rs
+++ src/main.rs
@@ -9,22 +9,24 @@ use raylib::prelude::*;
 
 fn main() {
     let dungeon = DungGen::new()
-        .width(100)
-        .height(100)
-        .n_rooms(20)
+        .width(150)
+        .height(150)
+        .n_rooms(120)
+        .room_min(10)
+        .room_range(15)
         .generate();
 
     // dungeon.print();
 
     let (mut rl, thread) = raylib::init()
-        .size(640, 480)
+        .size(1024, 768)
         .title("deeper")
         .build();
 
     let sq_width = 10;
 
-    let mut x_offset = 0.5 * -sq_width as f64 * dungeon.width as f64;
-    let mut y_offset = 0.5 * -sq_width as f64 * dungeon.height as f64;
+    let mut x_offset = 0.25 * -sq_width as f64 * dungeon.width  as f64;
+    let mut y_offset = 0.25 * -sq_width as f64 * dungeon.height as f64;
 
     while !rl.window_should_close() {
         let mut d = rl.begin_drawing(&thread);
@@ -32,11 +34,11 @@ fn main() {
         d.clear_background(Color::BLACK);
         d.draw_text("deeper", 12, 12, 30, Color::WHITE);
 
-        x_offset += 0.1;
+        x_offset += 0.3;
         y_offset += 0.2;
 
-        if x_offset >= 0.5 * (sq_width  * dungeon.width) as f64 { x_offset *= -1.0 }
-        if y_offset >= 0.5 * (sq_width  * dungeon.height) as f64 { y_offset *= -1.0 }
+        if x_offset >= (sq_width * dungeon.width)  as f64 { x_offset = -0.75 * (sq_width * dungeon.width)  as f64 }
+        if y_offset >= (sq_width * dungeon.height) as f64 { y_offset = -0.75 * (sq_width * dungeon.height) as f64 }
 
         for x in 0..=dungeon.height {
             for y in 0..=dungeon.width {
