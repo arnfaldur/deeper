09dfce3412c792bf381e8a546af59edd9c38a5b2
arnfaldur <aarnaldur@gmail.com>
2021-02-13 05:36:39 +0000

    EntityBuilder actually works, 1 week in 25 minutes

--- src/components/entity_builder.rs
+++ src/components/entity_builder.rs
@@ -14,32 +14,42 @@ use crate::components::*;
 
 pub struct EntityBuilder<'a> {
     buffer: legion::systems::CommandBuffer,
+    entity: Entity,
     world: &'a mut World,
 }
 
 impl<'a> EntityBuilder<'a> {
     pub fn new(world: &'a mut World) -> Self {
-        Self {
-            buffer: legion::systems::CommandBuffer::new(world),
+        let mut buffer = legion::systems::CommandBuffer::new(world);
+        let entity = buffer.push(());
+        return Self {
+            buffer,
+            entity,
             world,
-        }
+        };
     }
     pub fn build(&mut self) { self.buffer.flush(self.world) }
-    fn add_component<T: Component>(&mut self, component: T) { self.buffer.push((component,)); }
+    fn add_component<T: Component>(&mut self, component: T) {
+        self.buffer.add_component(self.entity, component);
+    }
     pub fn position(&mut self, pos: Vector2<f32>) -> &mut Self {
-        self.add_component::<Position>(Position(pos));
+        self.add_component(Position(pos));
         return self;
     }
     pub fn velocity(&mut self, vel: Vector2<f32>) -> &mut Self {
-        self.add_component::<Velocity>(Velocity(vel));
+        self.add_component(Velocity(vel));
+        return self;
+    }
+    pub fn orientation(&mut self, ori: f32) -> &mut Self {
+        self.add_component(Orientation(Deg(ori)));
         return self;
     }
     pub fn dynamic_body(&mut self, mass: f32) -> &mut Self {
-        self.add_component::<DynamicBody>(DynamicBody { mass });
+        self.add_component(DynamicBody { mass });
         return self;
     }
     pub fn agent(&mut self, accel: Acceleration) -> &mut Self {
-        self.add_component::<Acceleration>(accel);
+        self.add_component(accel);
         return self;
     }
 }
