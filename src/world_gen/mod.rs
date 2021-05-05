702bd58b5252a067f1e5c30795ebd524687e3a30
jokulhaups <jokullmani12@gmail.com>
2021-05-05 04:52:43 +0000

    fix warnings

--- src/world_gen/mod.rs
+++ src/world_gen/mod.rs
@@ -1,6 +1,3 @@
-#![feature(map_first_last)]
-#![feature(btree_retain)]
-
 pub mod components;
 mod dung_gen;
 pub mod systems;
--- src/world_gen/wfc.rs
+++ src/world_gen/wfc.rs
@@ -1,28 +1,17 @@
-use std::collections::hash_map::DefaultHasher;
-use std::collections::{BTreeMap, HashMap};
-use std::fmt::Debug;
-use std::hash::{Hash, Hasher};
-use std::iter::FromIterator;
-use std::num::NonZeroU32;
-use std::ops::{Index, IndexMut};
-use std::time::Instant;
-
-use bit_set::BitSet;
-use cgmath::{Array, Vector2, Zero};
-use image::{DynamicImage, ImageBuffer, Pixel, Rgb, RgbImage};
-use itertools::Itertools;
-use rand::prelude::*;
-
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use std::num::NonZeroU32;
+    use std::time::Instant;
+
+    use rand::prelude::StdRng;
+    use rand::SeedableRng;
+    use wfc_image::*;
 
     #[test]
     pub fn test() {
         let timer = Instant::now();
         let pic = image::open("assets/Images/dungeon_sample.bmp").unwrap();
         let mut rng = StdRng::from_entropy();
-        use wfc_image::*;
         if let Ok(out) = generate_image_with_rng(
             &pic,
             NonZeroU32::new(3).unwrap(),
